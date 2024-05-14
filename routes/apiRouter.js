0e12510d06dbfc8563af5a9e4c50c8d0ff31862e
Moyyor <149517391+Moyyor@users.noreply.github.com>
2024-05-14 13:57:30 +0300

    Добавил public и авторизацию

---
+++ routes/apiRouter.js
@@ -0,0 +1,13 @@
+const usersRouter = require('./users');
+const gamesRouter = require('./games');
+const categoriesRouter = require('./categories');
+const authRouter = require("./auth.js")
+
+const apiRouter = require("express").Router();
+
+apiRouter.use("/api", gamesRouter);
+apiRouter.use("/api", usersRouter);
+apiRouter.use("/api", categoriesRouter);
+apiRouter.use("/api", authRouter);
+
+module.exports = apiRouter;
