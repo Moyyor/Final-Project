14fc6de79f90c941efd9404b096518d238e77114
Moyyor <149517391+Moyyor@users.noreply.github.com>
2024-05-28 19:53:34 +0300

    app.js

--- app.js
+++ app.js
@@ -1,24 +1,23 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const path = require('path');
-const apiRouter = require('./routes/apiRouter');
-
-const cors = require("./middlewares/cors.js")
-const connectToDatabase = require('./database/connect');
-
-
-const PORT = 3000;
+const express = require("express");
+const bodyParser = require("body-parser");
+const path = require("path");
+const connectToDatabase = require("./database/connect");
+const cors = require("./middlewares/cors");
+const apiRouter = require("./routes/apiRouter");
+const cookieParser = require("cookie-parser");
+const pagesRouter = require("./routes/pages");
 const app = express();
+const PORT = 3001;
 
 connectToDatabase();
 
-app.use( 
+app.use(
   cors,
+  cookieParser(),
   bodyParser.json(),
-  express.static(path.join(__dirname, 'public')),
-  apiRouter
+  pagesRouter,
+  apiRouter,
+  express.static(path.join(__dirname, "public")),
 );
 
-app.listen(PORT, () => {
-    console.log(`Server is running at PORT http://localhost:${PORT}`);
-});
+app.listen(PORT);
