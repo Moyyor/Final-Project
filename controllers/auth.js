38007a019bcc8337e5ad85bbb252d6f0373445bd
Moyyor <149517391+Moyyor@users.noreply.github.com>
2024-05-28 19:42:44 +0300

    Печеньки программистов

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,4 +1,6 @@
 const users = require("../models/user.js");
+const jwt = require("jsonwebtoken");
+const path = require("path");
 
 const login = (req, res) => {
   const { email, password } = req.body;
@@ -6,13 +8,40 @@ const login = (req, res) => {
   users
     .findUserByCredentials(email, password)
     .then((user) => {
+      const token = jwt.sign({ _id: user._id }, "some-secret-key", {
+        expiresIn: 3600,
+      });
+      return { user, token };
+    })
+    .then(({ user, token }) => {
       res
         .status(200)
-        .send({ _id: user._id, username: user.username, email: user.email });
+        .send({
+          _id: user._id,
+          username: user.username,
+          email: user.email,
+          jwt: token,
+        });
     })
     .catch((error) => {
       res.status(401).send({ message: error.message });
     });
 };
 
-module.exports = { login };
+const sendIndex = (req, res) => {
+    if (req.cookies.jwt) {
+      try {
+        jwt.verify(req.cookies.jwt, "some-secret-key");
+        return res.redirect("/admin/dashboard");
+      } catch (err) {
+        res.sendFile(path.join(__dirname, "../public/index.html"));
+      }
+    }
+    res.sendFile(path.join(__dirname, "../public/index.html"));
+  };
+
+const sendDashboard = (req, res) => {
+    res.sendFile(path.join(__dirname, "../public/admin/dashboard.html"));
+  };
+
+module.exports = { login, sendIndex, sendDashboard };
