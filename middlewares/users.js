5de31f4e1c69db6c4c3f6094852bfd8c949123f4
Moyyor <149517391+Moyyor@users.noreply.github.com>
2024-05-28 19:50:57 +0300

    Регистрация пользователей completed

--- middlewares/users.js
+++ middlewares/users.js
@@ -1,16 +1,36 @@
 const users = require("../models/user");
 const bcrypt = require("bcryptjs");
 
+const hashPassword = async (req, res, next) => {
+  try {
+    const salt = await bcrypt.genSalt(10);
+    const hash = await bcrypt.hash(req.body.password, salt);
+    req.body.password = hash;
+    next();
+  } catch (error) {
+    res.status(400).send({ message: "Ошибка хеширования пароля" });
+  }
+}; 
+
 const findAllUsers = async (req, res, next) => {
   req.usersArray = await users.find({});
   next();
 };
 
-const createUser = async (req, res, next) => {
-  console.log("POST /users");
+const findUserById = async (req, res, next) => {
   try {
-    console.log(req.body);
+    req.user = await users.findById(req.params.id);
+    next();
+  } catch (error) {
+    res.setHeader("Content-Type", "application/json");
+    res.status(404).send(JSON.stringify({ message: "Пользователь не найден" }));
+  }
+};
+
+const createUser = async (req, res, next) => {
+  try {
     req.user = await users.create(req.body);
+
     next();
   } catch (error) {
     res.setHeader("Content-Type", "application/json");
@@ -28,7 +48,7 @@ const updateUser = async (req, res, next) => {
     res.setHeader("Content-Type", "application/json");
     res
       .status(400)
-      .send(JSON.stringify({ message: "Ошибка обновления пользователей" }));
+      .send(JSON.stringify({ message: "Ошибка обновления пользователя" }));
   }
 };
 
@@ -44,15 +64,64 @@ const deleteUser = async (req, res, next) => {
   }
 };
 
-const hashPassword = async (req, res, next) => {
-  try {
-    const salt = await bcrypt.genSalt(10);
-    const hash = await bcrypt.hash(req.body.password, salt);
-    req.body.password = hash;
+const checkEmptyNameAndEmailAndPassword = async (req, res, next) => {
+  if (!req.body.username || !req.body.email || !req.body.password) {
+    res.setHeader("Content-Type", "application/json");
+    res
+      .status(400)
+      .send(JSON.stringify({ message: "Введите имя, email и пароль" }));
+  } else {
     next();
-  } catch (error) {
-    res.status(400).send({ message: "Ошибка хеширования пароля" });
   }
 };
 
-module.exports = { createUser, findAllUsers, updateUser, deleteUser, hashPassword };
+const checkEmptyNameAndEmail = async (req, res, next) => {
+  if (!req.body.username || !req.body.email) {
+    res.setHeader("Content-Type", "application/json");
+    res.status(400).send(JSON.stringify({ message: "Введите имя и email" }));
+  } else {
+    next();
+  }
+};
+
+const checkIsUserExists = async (req, res, next) => {
+  const isInArray = req.usersArray.find((user) => {
+    return req.body.email === user.email;
+  });
+  if (isInArray) {
+    res.setHeader("Content-Type", "application/json");
+    res
+      .status(400)
+      .send(
+        JSON.stringify({ message: "Пользователь с таким email уже существует" })
+      );
+  } else {
+    next();
+  }
+};
+
+const filterPassword = (req, res, next) => {
+  const filterUser = (user) => {
+    const { password, ...userWithoutPassword } = user.toObject();
+    return userWithoutPassword;
+  };
+  if (req.user) {
+    req.user = filterUser(req.user);
+  }
+  if (req.usersArray) {
+    req.usersArray = req.usersArray.map(filterUser);
+  }
+  next();
+};
+module.exports = {
+  findAllUsers,
+  createUser,
+  findUserById,
+  updateUser,
+  deleteUser,
+  checkEmptyNameAndEmailAndPassword,
+  checkEmptyNameAndEmail,
+  checkIsUserExists,
+  filterPassword,
+  hashPassword,
+};
