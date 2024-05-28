46881c22b133a5259d9c0e2379380ef84faa18da
Moyyor <149517391+Moyyor@users.noreply.github.com>
2024-05-28 19:50:16 +0300

    Запускаем pindie completed

--- routes/games.js
+++ routes/games.js
@@ -1,11 +1,48 @@
-const gamesRouter = require('express').Router();
+const gamesRouter = require("express").Router();
+const { checkAuth } = require("../middlewares/auth");
+const {
+  findAllGames,
+  createGame,
+  findGameById,
+  updateGame,
+  deleteGame,
+  checkEmptyFields,
+  checkIfCategoriesAvaliable,
+  checkIfUsersAreSafe,
+  checkIsGameExists,
+  checkIsVoteRequest,
+} = require("../middlewares/games");
+const {
+  sendAllGames,
+  sendGameCreated,
+  sendGameById,
+  sendGameUpdated,
+  sendGameDeleted,
+} = require("../controllers/games");
 
-const { findAllGames, createGame, updateGame, deleteGame } = require('../middlewares/games');
-const { sendAllGames, sendGameCreated, sendGameUpdated, sendGameDeleted } = require('../controllers/games');
-
-gamesRouter.get('/games', findAllGames, sendAllGames);
-gamesRouter.post('/games', createGame, sendGameCreated);
-gamesRouter.put("/games/:id", updateGame, sendGameUpdated);
-gamesRouter.delete("/games/:id", deleteGame, sendGameDeleted);
+gamesRouter.get("/games", findAllGames, sendAllGames);
+gamesRouter.post(
+  "/games",
+  checkAuth,
+  findAllGames,
+  checkEmptyFields,
+  checkIfCategoriesAvaliable,
+  checkIsGameExists,
+  createGame,
+  sendGameCreated
+);
+gamesRouter.get("/games/:id", findGameById, sendGameById);
+gamesRouter.put(
+  "/games/:id",
+  checkAuth,
+  findGameById,
+  checkIsVoteRequest,
+  checkIfUsersAreSafe,
+  checkIfCategoriesAvaliable,
+  checkEmptyFields,
+  updateGame,
+  sendGameUpdated
+);
+gamesRouter.delete("/games/:id", checkAuth, deleteGame, sendGameDeleted);
 
 module.exports = gamesRouter;
