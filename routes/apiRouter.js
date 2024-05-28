9b95669de0afe5124fa7984913405bcb7ca827d6
Moyyor <149517391+Moyyor@users.noreply.github.com>
2024-05-28 19:53:16 +0300

    Авторизация completed

--- routes/apiRouter.js
+++ routes/apiRouter.js
@@ -1,13 +1,12 @@
-const usersRouter = require('./users');
-const gamesRouter = require('./games');
-const categoriesRouter = require('./categories');
-const authRouter = require("./auth.js")
-
 const apiRouter = require("express").Router();
+const gamesRouter = require("./games");
+const usersRouter = require("./users");
+const categoriesRouter = require("./categories")
+const authRouter = require("./auth")
 
 apiRouter.use("/api", gamesRouter);
 apiRouter.use("/api", usersRouter);
-apiRouter.use("/api", categoriesRouter);
-apiRouter.use("/api", authRouter);
+apiRouter.use("/api", categoriesRouter)
+apiRouter.use("/api", authRouter)
 
 module.exports = apiRouter;
