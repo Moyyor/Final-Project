369007cb8c47a8432f804261d1740c4a19560e99
Moyyor <149517391+Moyyor@users.noreply.github.com>
2024-05-28 19:43:43 +0300

    D - delete completed

--- controllers/categories.js
+++ controllers/categories.js
@@ -1,21 +1,32 @@
 const sendAllCategories = (req, res) => {
-  res.setHeader('Content-Type', 'application/json');
+  res.setHeader("Content-Type", "application/json");
   res.end(JSON.stringify(req.categoriesArray));
+};
+
+const sendCategoryById = (req, res) => {
+  res.setHeader("Content-Type", "application/json");
+  res.end(JSON.stringify(req.category));
 };
 
 const sendCategoryCreated = (req, res) => {
   res.setHeader("Content-Type", "application/json");
-  res.end(JSON.stringify(req.game));
+  res.end(JSON.stringify(req.category));
 };
 
 const sendCategoryUpdated = (req, res) => {
   res.setHeader("Content-Type", "application/json");
-  res.status(200).send(JSON.stringify({ message: "Category updated" }));
+  res.status(200).send(JSON.stringify({ message: "Категория обновлена" }));
 };
 
 const sendCategoryDeleted = (req, res) => {
   res.setHeader("Content-Type", "application/json");
-  res.end(JSON.stringify(req.game));
-  };
+  res.end(JSON.stringify(req.category));
+};
 
-module.exports = {sendCategoryCreated, sendAllCategories, sendCategoryUpdated, sendCategoryDeleted};
+module.exports = {
+  sendAllCategories,
+  sendCategoryCreated,
+  sendCategoryById,
+  sendCategoryUpdated,
+  sendCategoryDeleted,
+};
